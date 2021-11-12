ea0d1a3cdaf918b79a4f68fdded6ffa684284fc2
Moraru <cristina.moraru@atos.net>
2021-11-12 18:05:16 +0200

    Add project files.

---
+++ ArithmeticProgression/CheckAP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArithmeticProgression
+{
+    public class CheckAP
+    {
+        public static bool IsArithmeticProgression(List<int> numbers)
+        {
+            numbers.Sort();
+            var diff = numbers[1] - numbers[0];
+            //Console.WriteLine($"The difference is {diff}");
+
+            for (var index = 2; index < numbers.Count; index++)
+            {
+               if (numbers[index] - numbers[index - 1] != diff)
+               {
+
+                    return false;
+                    
+               }
+
+                      
+              
+            }
+            return true; 
+        }
+        
+    }
+}
---
+++ ArithmeticProgression/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArithmeticProgression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            Console.WriteLine("Please enter the number of elements for the sequence (greater than 4):");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n >= 4)
+            {
+                Console.WriteLine("Please enter the elements:");
+                for (var index = 0; index < n; index++)
+                {
+                    int x=int.Parse(Console.ReadLine());
+                    numbers.Add(x);
+                }
+
+                if (CheckAP.IsArithmeticProgression(numbers) == true)
+                {
+                    Console.WriteLine("YES");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    Console.ReadLine();
+                }
+                
+            }
+            else Console.WriteLine("Sorry, the number you provided is less than 4");
+        }
+    }
+}
